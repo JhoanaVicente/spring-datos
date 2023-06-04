9addfe77bd332b2d8735a5d7d60c50575f0f333d
JhoanaVicente <126072279+JhoanaVicente@users.noreply.github.com>
2023-06-04 20:27:12 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/sprindatos/SprinDatosApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sprindatos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprinDatosApplication
+
+fun main(args: Array<String>) {
+    runApplication<SprinDatosApplication>(*args)
+}
